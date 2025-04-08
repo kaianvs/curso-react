774ae654a3841c7e4ad0443be2054392bfb10a55
Kaianvs <kaianvs12@gmail.com>
2025-04-07 21:33:00 -0300

    feat: create Pokemon component and create pages and components folders

---
+++ src/components/Pokemon.jsx
@@ -0,0 +1,7 @@
+export const Pokemon = ()=> {
+    return (
+     <h2>
+        Nome do pokemon: pikachu
+     </h2>
+    );
+  }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import { Pokemon } from "../components/Pokemon";
+
+export const Home = ()=> {
+  return (
+
+    <div>
+
+      <h1>
+         Meu site React
+      </h1>
+      <Pokemon />
+
+    </div>
+  );
+}
+
+export default Home;
